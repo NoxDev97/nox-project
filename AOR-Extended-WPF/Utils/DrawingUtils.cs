7c02a0736a6e957c4b07129e56bea5fbde23260b
NoxDev97 <noxtvoficial@gmail.com>
2024-06-20 11:52:05 -0300

    ok

--- AOR-Extended-WPF/Utils/DrawingUtils.cs
+++ AOR-Extended-WPF/Utils/DrawingUtils.cs
@@ -3,6 +3,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
 
 namespace AOR_Extended_WPF.Utils
 {
